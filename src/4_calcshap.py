75e7e7c98364642fd3b5eb74d24149da17e1d0c7
Mariekev93 <mvinkenoog@gmail.com>
2022-07-13 14:59:58 +0200

    add code to calculate shap values

---
+++ src/4_calcshap.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pandas as pd 
+import datetime
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import shap
+import pickle
+from sklearn.svm import SVC
+import sys
+import datetime
+
+import warnings 
+warnings.filterwarnings('ignore')
+
+sex = sys.argv[1]
+nback = sys.argv[2]
+foldersuffix = sys.argv[3]
+n = sys.argv[4]
+
+def calc_shap(nback, sex, n=100):
+    filename = '/home/vinkenoogm/SVM-NL/results/models'+foldersuffix+'/clf_' + str(nback) + '.sav'
+    clf = pickle.load(open(filename, 'rb'))
+    
+    index = 0 if sex == 'men' else 1
+    val = pd.read_pickle('/data1/vinkenoogm/scaled'+foldersuffix+'/'+str(sex)+'_'+str(nback)+'_test.pkl')
+    clf_s = clf[index]
+    X_val = val[val.columns[:-1]]
+    X_shap = shap.sample(X_val, n)
+    explainer = shap.KernelExplainer(clf_s.predict, X_shap)
+    shapvals = explainer.shap_values(X_shap)
+        
+    path = '/home/vinkenoogm/SVM-NL/results/shap'+foldersuffix+'/'
+    filename1 = 'Xshap_' + sex + '_' + str(nback) + '_' + str(n) + '.pkl'
+    filename2 = 'shapvals_' + sex + '_' + str(nback) + '_' + str(n) + '.pkl'
+        
+    pickle.dump(X_shap, open(path+filename1, 'wb'))
+    pickle.dump(shapvals, open(path+filename2, 'wb'))
+        
+print('Calculating shap values for ', sex, ' nback', nback, '( N =',  n, ') starting at', datetime.datetime.now())
+calc_shap(nback, sex, int(n))
