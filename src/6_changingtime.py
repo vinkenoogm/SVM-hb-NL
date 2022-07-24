20a7822bfba29291ab41a190d3191fd0c96b289c
Sander van Rijn <svr003@gmail.com>
2022-07-24 20:48:50 +0200

    applying f-strings and pathlib.Path to 6_

--- src/6_changingtime.py
+++ src/6_changingtime.py
@@ -1,18 +1,16 @@
-import numpy as np
-import pandas as pd 
-import datetime
+import pandas as pd
+from pathlib import Path
 import pickle
 import sys
-from sklearn.model_selection import KFold
-from sklearn.metrics import classification_report
-from sklearn.svm import SVC
-from sklearn.preprocessing import StandardScaler
-from sklearn.compose import ColumnTransformer
+
+data_path = Path('/data1/vinkenoogm/')
+result_path = Path('/home/vinkenoogm/SVM-NL/results')
 
 sex = sys.argv[1]
 nback = int(sys.argv[2])
 foldersuffix = sys.argv[3]
 print(sex, nback, foldersuffix)
+
 
 def make_preds(data, clf):
     X = data[data.columns[:-1]]
@@ -20,10 +18,11 @@ def make_preds(data, clf):
     y_pred = clf.predict(X)
     return(y_pred)
 
-data = pd.read_pickle('/data1/vinkenoogm/scaled'+foldersuffix+'/'+str(sex)+'_'+str(nback)+'_test.pkl')
-clf = pickle.load(open('/home/vinkenoogm/SVM-NL/results/models'+foldersuffix+'/clf_' + str(nback) + '.sav', 'rb'))
+
+data = pd.read_pickle(data_path / f'scaled{foldersuffix}/{sex}_{nback}_test.pkl')
+clf = pickle.load(open(result_path / f'models{foldersuffix}/clf_{nback}.sav', 'rb'))
 clf_s = clf[0] if sex == 'men' else clf[1]
-scaler = pickle.load(open('/home/vinkenoogm/SVM-NL/results/scalers'+foldersuffix+'/'+sex+'_'+str(nback)+'.pkl', 'rb'))
+scaler = pickle.load(open(result_path / f'scalers{foldersuffix}/{sex}_{nback}.pkl', 'rb'))
 
 print('loaded data')
 
@@ -31,10 +30,7 @@ data_res = data.copy()
 y_pred_first = make_preds(data, clf_s)
 data_res['HbOK_pred'] = y_pred_first
 
-timecols = ['TimetoFer']
-for n in range(1, nback+1):
-    timecols.extend(['TimetoPrev'+str(n)])
-
+timecols = ['TimetoFer', *[f'TimetoPrev{str(n)}' for n in range(1, nback+1)]]
 for timestep in range(-364, 371, 7):
     print(timestep)
     data_timestep = data.copy()
@@ -46,11 +42,11 @@ for timestep in range(-364, 371, 7):
 
     y_pred = make_preds(data_timestep, clf_s)
 
-    varname = 'HbOK_pred_' + str(timestep)
+    varname = f'HbOK_pred_{timestep}'
     data_res[varname] = y_pred
     if timestep == 0:
         data_res = data_res.copy()
 
 print(data_res.head())
-path = '/data1/vinkenoogm/pred_timechange'+foldersuffix+'/'        
-data_res.to_pickle(path + 'data_res_' + sex + '_' + str(nback) + '.pkl')
+path = data_path / f'pred_timechange{foldersuffix}/'
+data_res.to_pickle(path / f'data_res_{sex}_{nback}.pkl')
